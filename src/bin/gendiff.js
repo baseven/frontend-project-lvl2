6d509c4daf921f02aa2655f20dcaa3734d36d09f
Vladislav Akashev <vlad.akashev.w@gmail.com>
2019-06-25 17:54:41 +0300

    add ".action" func

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,4 +7,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
+  .action((firstFile, secondFile) => {
+    console.log('The difference between the first and second file:');
+    console.log(`${firstFile}`);
+    console.log(`${secondFile}`);
+  })
   .parse(process.argv);
