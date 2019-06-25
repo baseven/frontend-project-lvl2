ebc34260e094a6786db85c34924723d508ef2bd0
Vladislav Akashev <vlad.akashev.w@gmail.com>
2019-06-25 20:22:46 +0300

    add babel-eslint

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,8 +8,6 @@ program
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
   .action((firstFile, secondFile) => {
-    console.log('The difference between the first and second file:');
-    console.log(`${firstFile}`);
-    console.log(`${secondFile}`);
+    console.log(`The function that handles the values of the transferred files (${firstFile}, ${secondFile}) should be located here.`);
   })
   .parse(process.argv);
