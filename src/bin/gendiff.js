018eb5f5782b57b146f0984fe3b6cdea8dca11db
Vladislav Akashev <vlad.akashev.w@gmail.com>
2019-07-07 12:33:15 +0300

    updated version of gendiif to 0.1.0

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import makeDiff from '..';
 
 program
-  .version('0.0.0')
+  .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format', 'default')
