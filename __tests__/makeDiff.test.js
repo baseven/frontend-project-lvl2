2cd3286c59a099f877f46b9c43390ab9c2dc8406
Vladislav Akashev <vlad.akashev.w@gmail.com>
2019-07-16 11:20:50 +0300

    code refactoring based on mentor comments

--- __tests__/makeDiff.test.js
+++ __tests__/makeDiff.test.js
@@ -7,33 +7,27 @@ const getPathToFile = (extension, name) => `${path}__${extension}__/${name}.${ex
 
 const getTestResult = filename => fs.readFileSync(`${path}__testResult__/${filename}`, 'utf8');
 
-test.each([
-  [`${getPathToFile('json', 'before')}`, `${getPathToFile('json', 'after')}`, getTestResult('pretty.txt')],
-  [`${getPathToFile('yml', 'before')}`, `${getPathToFile('yml', 'after')}`, getTestResult('pretty.txt')],
-  [`${getPathToFile('ini', 'before')}`, `${getPathToFile('ini', 'after')}`, getTestResult('pretty.txt')],
-])(
+const getTestParameters = (extension, filename) => [getPathToFile(extension, 'before'), getPathToFile(extension, 'after'), getTestResult(filename)];
+
+const arrayOfExtensions = ['json', 'yml', 'ini'];
+
+const getTestTable = (extensions, filename) => extensions.map(v => getTestParameters(v, filename));
+
+test.each(getTestTable(arrayOfExtensions, 'pretty.txt'))(
   'makeDiff using pretty output format',
   (pathToFileBefore, pathToFileAfter, testResult) => {
     expect(makeDiff(pathToFileBefore, pathToFileAfter)).toBe(testResult);
   },
 );
 
-test.each([
-  [`${getPathToFile('json', 'before')}`, `${getPathToFile('json', 'after')}`, getTestResult('plain.txt')],
-  [`${getPathToFile('yml', 'before')}`, `${getPathToFile('yml', 'after')}`, getTestResult('plain.txt')],
-  [`${getPathToFile('ini', 'before')}`, `${getPathToFile('ini', 'after')}`, getTestResult('plain.txt')],
-])(
+test.each(getTestTable(arrayOfExtensions, 'plain.txt'))(
   'makeDiff using plain output format',
   (pathToFileBefore, pathToFileAfter, testResult) => {
     expect(makeDiff(pathToFileBefore, pathToFileAfter, 'plain')).toBe(testResult);
   },
 );
 
-test.each([
-  [`${getPathToFile('json', 'before')}`, `${getPathToFile('json', 'after')}`, getTestResult('json.json')],
-  [`${getPathToFile('yml', 'before')}`, `${getPathToFile('yml', 'after')}`, getTestResult('json.json')],
-  [`${getPathToFile('ini', 'before')}`, `${getPathToFile('ini', 'after')}`, getTestResult('json.json')],
-])(
+test.each(getTestTable(arrayOfExtensions, 'json.json'))(
   'makeDiff using json output format',
   (pathToFileBefore, pathToFileAfter, testResult) => {
     expect(makeDiff(pathToFileBefore, pathToFileAfter, 'json')).toBe(testResult);
