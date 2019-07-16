1a17ecb5924b04616c3f2f8b28c8cb6d204fee34
Vladislav Akashev <vlad.akashev.w@gmail.com>
2019-07-16 11:52:29 +0300

    code refactoring based on mentor comments

--- __tests__/makeDiff.test.js
+++ __tests__/makeDiff.test.js
@@ -3,33 +3,28 @@ import makeDiff from '../src';
 
 const path = `${__dirname}/__fixtures__/__recursiveStructure__/`;
 
+const arrayOfExtensions = ['json', 'yml', 'ini'];
+const arrayOfFormats = ['pretty', 'plain', 'json'];
+
 const getPathToFile = (extension, name) => `${path}__${extension}__/${name}.${extension}`;
 
 const getTestResult = filename => fs.readFileSync(`${path}__testResult__/${filename}`, 'utf8');
 
 const getTestParameters = (extension, filename) => [getPathToFile(extension, 'before'), getPathToFile(extension, 'after'), getTestResult(filename)];
 
-const arrayOfExtensions = ['json', 'yml', 'ini'];
-
 const getTestTable = (extensions, filename) => extensions.map(v => getTestParameters(v, filename));
 
-test.each(getTestTable(arrayOfExtensions, 'pretty.txt'))(
-  'makeDiff using pretty output format',
-  (pathToFileBefore, pathToFileAfter, testResult) => {
-    expect(makeDiff(pathToFileBefore, pathToFileAfter)).toBe(testResult);
-  },
-);
+const makeTest = (extensions, format) => {
+  const filename = format === 'json' ? 'json.json' : `${format}.txt`;
 
-test.each(getTestTable(arrayOfExtensions, 'plain.txt'))(
-  'makeDiff using plain output format',
-  (pathToFileBefore, pathToFileAfter, testResult) => {
-    expect(makeDiff(pathToFileBefore, pathToFileAfter, 'plain')).toBe(testResult);
-  },
-);
+  test.each(getTestTable(extensions, filename))(
+    `makeDiff using ${format} output format`,
+    (pathToFileBefore, pathToFileAfter, testResult) => {
+      expect(makeDiff(pathToFileBefore, pathToFileAfter, format)).toBe(testResult);
+    },
+  );
+};
 
-test.each(getTestTable(arrayOfExtensions, 'json.json'))(
-  'makeDiff using json output format',
-  (pathToFileBefore, pathToFileAfter, testResult) => {
-    expect(makeDiff(pathToFileBefore, pathToFileAfter, 'json')).toBe(testResult);
-  },
-);
+const makeTestSuites = (extensions, formats) => formats.map(format => makeTest(extensions, format));
+
+makeTestSuites(arrayOfExtensions, arrayOfFormats);
